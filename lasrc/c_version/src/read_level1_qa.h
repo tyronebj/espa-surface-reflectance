138516dce1a3698bc2d4badd92347f793985f9e4
Gail Schmidt <gail.schmidt.ctr@usgs.gov>
2020-06-17 08:13:36 -0500

    Added the new header files needed for this source code

---
+++ lasrc/c_version/src/read_level1_qa.h
@@ -0,0 +1,43 @@
+#ifndef _READ_LEVEL1_QA_H_
+#define _READ_LEVEL1_QA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Defines for the Level-1 pixel QA band */
+/* Define the constants used for shifting bits and ANDing with the bits to
+   get to the desire quality bits */
+#define ESPA_L1_SINGLE_BIT 0x01             /* 00000001 */
+#define ESPA_L1_DOUBLE_BIT 0x03             /* 00000011 */
+#define ESPA_L1_DESIGNATED_FILL_BIT 0       /* one bit */
+
+/******************************************************************************
+MODULE:  level1_qa_is_fill
+
+PURPOSE: Determines if the current Level-1 QA pixel is fill
+
+RETURN VALUE:
+Type = boolean
+Value           Description
+-----           -----------
+true            Pixel is fill
+false           Pixel is not fill
+
+NOTES:
+1. This is an inline function so it should be fast as the function call overhead
+   is eliminated by dropping the code inline with the original application.
+******************************************************************************/
+static inline bool level1_qa_is_fill
+(
+    uint16_t l1_qa_pix      /* I: Level-1 QA value for current pixel */
+)
+{
+    if (((l1_qa_pix >> ESPA_L1_DESIGNATED_FILL_BIT) & ESPA_L1_SINGLE_BIT) == 1)
+        return true;
+    else
+        return false;
+}
+
+#endif
